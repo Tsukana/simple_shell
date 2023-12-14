61153deec893fcf72ad4d14ff79d1890f6996a4e
tsukana <soukaina.farhan911@gmail.com>
2023-12-13 16:11:41 -0800

    simple_shell

---
+++ main.c
@@ -0,0 +1,43 @@
+#include "shell.h"
+
+/**
+ * main - entry point
+ * @argc: arg count
+ * @argv: arg vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char **argv)
+{
+	std_infos info[] = { INFO_INIT };
+	int fd = 2;
+
+	asm ("mov %1, %0\n\t"
+			"add $3, %0"
+			: "=r" (fd)
+			: "r" (fd));
+
+	if (argc == 2)
+	{
+		fd = open(argv[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				put_err(argv[0]);
+				put_err(": 0: Can't open ");
+				put_err(argv[1]);
+				putchar_err('\n');
+				putchar_err(BUFFLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
+	}
+	initEnvParams(info);
+	runShell(info, argv);
+	return (EXIT_SUCCESS);
+}
